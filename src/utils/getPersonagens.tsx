c2b4a11b16b4eb8ffe47a0a889a56f24a705436b
Wagner Araújo <wagneraraujo15@gmail.com>
2023-07-22 14:32:39 -0300

    create fn get personagens with return jsx

--- src/utils/getPersonagens.tsx
+++ src/utils/getPersonagens.tsx
@@ -1,18 +1,26 @@
-import {fetch} from "next/dist/compiled/@edge-runtime/primitives";
-import {useState} from "react";
+import { CardPersonagem } from 'components/cardPersonagem';
+import { ReactNode } from 'react';
 
-export default function getPersonagens() {
-    const [personagens, setPersonagens] = useState<string | any>({})
-    const [loadingPersonagens, setLoadingPersonagens] = useState(false)
+// const response = await fetch('https://swapi.dev/api/people');
 
-    async function apiStartWars(){
-        const response = await fetch('https://swapi.dev/api/people');
-        if(response){
-            const dados = await response?.json()
-            setPersonagens(dados?.results);
-        }
-    }
-    return {
-        personagens
-    }
+export interface getItems {
+    name: string;
+    gender?: string;
+    birth_year?: string;
+}
+
+export async function GetPersonagens() {
+    const response = await fetch('https://swapi.dev/api/people');
+    const personagens = await response.json();
+    console.log(personagens);
+
+    return personagens?.results.map((item: getItems) => {
+        return (
+            <CardPersonagem
+                name={item.name}
+                birth_year={item.birth_year}
+                gender={item.gender}
+            />
+        );
+    });
 }
